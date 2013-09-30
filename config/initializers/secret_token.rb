449e08afffdcf8c4fe7e2e8926d20d04c690286b
johnny <johnny@lionni.(none)>
2013-09-30 23:31:27 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Votefolio::Application.config.secret_key_base = '3098f7a2bf4e2ee35e961d6d6fe064c04077241a6ca76f7fa6bc7097ccb05b1f38e156c7474ca228babd721f4107b08ad5586bec16b25c8811eac4e8bd41d710'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Votefolio::Application.config.session_store :cookie_store, key: '_votefolio_session'
